15c6e9a400196be4af61c13d9c5aa94e0ea90c38
Lepidus151 <62634742+Lepidus151@users.noreply.github.com>
2025-01-05 13:01:49 +0900

    Delete project directory

--- project/js/models/Product.js
+++
@@ -1,7 +0,0 @@
-export class Product {
-  constructor(url, title, quantity = 1) {
-    this.url = url;
-    this.title = title;
-    this.quantity = quantity;
-  }
-}
--- project/js/utils/urlChecker.js
+++
@@ -1,7 +0,0 @@
-export const isCartPage = (url) => {
-  return url.includes('/cart/') || url.includes('/gp/cart/');
-};
-
-export const isCheckoutPage = (url) => {
-  return url.includes('/checkout/') || url.includes('/buy/');
-};
